5ac06052e6a2d1553c2f705d8aa98ef0af51c5ce
Tutku Kaya <69057794+tutku1@users.noreply.github.com>
2024-07-12 01:30:27 +0300

    Add files via upload

---
+++ odev3SQL.sql
@@ -0,0 +1,7 @@
+SELECT * FROM country WHERE country LIKE 'A%a';
+
+SELECT * FROM country WHERE country LIKE '______%n'
+
+SELECT * FROM film WHERE title ILIKE '%T%';
+
+SELECT * FROM film WHERE title LIKE  'C%' AND length > 90 AND rental_rate = 2.99;
